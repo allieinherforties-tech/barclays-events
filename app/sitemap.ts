2822018fd1ce682c37124d483f4daf4aa7aa4934
allieinherforties-tech <allieinherforties@gmail.com>
2025-11-03 11:33:05 -0500

    Update sitemap.ts
    barclays-events

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://barclaystonight.vercel.app/sitemap.xml',
+      url: 'https://barclays-events.vercel.app/sitemap.xml',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
