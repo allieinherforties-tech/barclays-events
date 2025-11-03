42c57f66c4c19c2770732bb0af3df195830a4c11
allieinherforties-tech <allieinherforties@gmail.com>
2025-11-03 11:26:58 -0500

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://barclaystonight.com',
+      url: 'https://barclaystonight.com/sitemap.xml',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
