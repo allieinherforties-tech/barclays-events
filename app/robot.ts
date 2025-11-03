a53546a34d85c69751c791be8c16174ed0749833
allieinherforties-tech <allieinherforties@gmail.com>
2025-11-03 11:32:46 -0500

    Update robot.ts
    barclays events

--- app/robot.ts
+++ app/robot.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://barclaystonight.vercel.app/robots.txt',
+    sitemap: 'https://barclays-events.vercel.app/robots.txt',
   }
 }
