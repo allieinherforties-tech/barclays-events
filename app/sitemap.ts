cefd52b7aef3988151d1400c7633fda31d52e688
allieinherforties-tech <allieinherforties@gmail.com>
2025-11-03 11:21:45 -0500

    Create sitemap.ts
    for SEO

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://barclaystonight.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ]
+}
